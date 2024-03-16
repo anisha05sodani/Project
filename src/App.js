806fc1d0e4cf9795bd1b14e84f708d6824805cfe
Anisha Sodani <ui22cs07@iiitsurat.ac.in>
2024-03-17 00:48:50 +0530

    z

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import React from 'react'
 import Navbar from './Components/Navbar/Navbar';
+import LoginForm from './Components/LoginForm/LoginForm'
 
 const App = () => {
   return (
     <div className='app'>
-      <Navbar />
-      {/* <LoginForm /> */}
+      {/* <Navbar /> */}
+      <LoginForm />
     </div>
   );
 }
